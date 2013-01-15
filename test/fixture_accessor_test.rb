a79787e5be9ad54b64a59d76abf19d7634e4115f
Robert Evans <robert@codewranglers.org>
2013-01-14 17:32:25 -0800

    rake test added

--- test/fixture_accessor_test.rb
+++ test/fixture_accessor_test.rb
@@ -1,19 +1,19 @@
 require 'minitest/autorun'
 require "./lib/fixture_overlord"
 
-class Game < Struct.new(:name, :points, :placement)
-  def self.create!(hash)
-    game = new(*hash)
-    game.id = 1
-    game
-  end
-end
+# class Game < Struct.new(:name, :points, :placement)
+#   def self.create!(hash)
+#     game = new(*hash)
+#     game.id = 1
+#     game
+#   end
+# end
 
-class TestGame
-  include FixtureOverlord
-  fixture_overlord :rule
-end
+# class TestGame
+#   include FixtureOverlord
+#   fixture_overlord :rule
+# end
 
-module FixtureOverlord
+# module FixtureOverlord
 
-end
+# end
